9f61308185d795919f79b85ae8e42c6c968547eb
How Yu Chern <yhow0004@student.monash.edu>
2022-05-24 23:47:33 +0800

    Assignment 2 - Verion 1 Date: 24/5/2022
    Notes:
    1. Initial Commit

---
+++ Assignments/Ass2/T1-rm-schema.sql
@@ -0,0 +1,36 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--T1-rm-schema.sql
+
+--Student ID: 31189733
+--Student Name: How Yu Chern
+--Unit Code: FIT3171
+--Applied Class No: Tutorial 04
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+-- Task 1 Add Create table statements for the Missing TABLES below
+-- Ensure all column comments, and constraints (other than FK's)
+-- are included. FK constraints are to be added at the end of this script
+
+-- COMPETITOR
+
+
+
+-- EMERCONTACT
+
+
+
+--ENTRY
+
+
+
+--TEAM
+
+
+
+-- Add all missing FK Constraints below here
